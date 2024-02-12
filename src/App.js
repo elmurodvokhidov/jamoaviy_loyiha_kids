2527779ab6a9eaa798c50b57644b71928b04e789
javohir <dejavohirrich1@gmail.com>
2024-02-12 10:18:27 +0300

    updated from javokhir

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
-      <h1>Hello from Kamronbek Sultanov 👋</h1>
+      <h1>Hello from javohir 👋</h1>
     </div>
   );
 }
